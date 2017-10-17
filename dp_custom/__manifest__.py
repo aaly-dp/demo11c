e086f5bd79402e59e3a70b01023f9414fdfa47d4
Ahmed Aly <ahmed.aly@datenpol.at>
2017-10-17 16:50:36 +0200

    Test external dependencies

---
+++ dp_custom/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# noinspection PyStatementEffect
+{
+    'name': 'datepol Report-Anpassungen',
+    'category': 'Custom',
+    'version': '1.0',
+    'summary': """Individuelle Report Anpassungen""",
+    'description': """Individuelle Report Anpassungen""",
+    'author': 'datenpol gmbh',
+    'website': 'http://www.datenpol.at/',
+    'depends': [
+        'base'
+    ],
+    'external_dependencies': {'python': ['unicodecsv']},
+    'data': [
+    ],
+    # only loaded in demonstration mode
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
